c155fff981b6d9a4ad5e5897c63fc33c48d05f69
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-06-10 20:50:35 -0300

    POST /participants validation with joy

--- src/index.js
+++ src/index.js
@@ -1,29 +1,86 @@
 import express from "express";
 import cors from "cors";
 import fs from "fs";
+import Joi from "joi";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const participants = [{ name: "ptrk", lastStatus: 12313123 }];
-const messages = [];
+const participants = [
+    { name: "Vader", lastStatus: 12313123 },
+    { name: "Yoda", lastStatus: 12313123 },
+];
+const messages = [
+    {
+        from: "Vader",
+        to: "Luke",
+        text: "Eu sou seu pai!",
+        type: "message",
+        time: "20:04:37",
+    },
+    {
+        from: "Luke",
+        to: "Todos",
+        text: "Num pod c",
+        type: "message",
+        time: "20:05:10",
+    },
+    {
+        from: "Obi Wan",
+        to: "Luke",
+        text: "Hello there!",
+        type: "private_message",
+        time: "20:06:14",
+    },
+    {
+        from: "Luke",
+        to: "Todos",
+        text: "Num pod c",
+        type: "message",
+        time: "20:07:23",
+    },
+];
 
 app.post("/participants", (req, res) => {
-    let newName = "";
-    if (req.body.name === undefined || !req.body.name.trim()) {
-        res.status(400).send("'Nome' não pode estar vazio.");
-        return;
+    const schema = Joi.object({
+        name: Joi.string().required().trim(),
+    });
+    const { error, validated } = schema.validate(req.body, {
+        abortEarly: false,
+    });
+    if (error) {
+        const errorMsgs = error.details.map((e) => e.message);
+        res.status(400).send(errorMsgs);
     } else {
-        newName = req.body.name.trim();
-    }
-    if (participants.find((p) => p.name === newName)) {
-        res.status(409).send("Esse nome já está sendo utilizado.");
-    } else {
-        participants.push({ name: newName, lastStatus: Date.now() });
-        res.send("OK");
+        console.log(validated);
+        if (participants.find((p) => p.name === req.body.name.trim())) {
+            res.status(409).send("Esse nome já está sendo utilizado.");
+        } else {
+            participants.push({
+                name: req.body.name.trim(),
+                lastStatus: Date.now(),
+            });
+            res.send("OK");
+        }
     }
 });
+
+app.get("/participants", (req, res) => {
+    res.send(participants);
+});
+
+// app.post("/messages", (req, res) => {
+//
+// });
+
+// app.get("/messages", (req, res) => {
+//
+// });
+
+// app.post("/status", (req, res) => {
+//
+// });
 
 app.listen(4000, () => {
     console.log("Running on port 4000");
