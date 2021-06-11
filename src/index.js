d771741ebaf8550c18e9b87eb0f5f788f1ac43d3
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-06-11 03:49:01 -0300

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
 import fs from "fs";
-import Joi from "joi";
 import dayjs from "dayjs";
+import {
+    trimAndClean,
+    checkUserError,
+    checkNameError,
+    checkNewMsgError,
+} from "../src/functions/validation.js";
 
 const chatData = fs.existsSync("./data/chatData.json")
     ? JSON.parse(fs.readFileSync("./data/chatData.json"))
@@ -15,31 +20,17 @@ app.use(express.json());
 let participants = chatData.participants;
 let messages = chatData.messages;
 
-scheduleParticipantsUpdate();
+scheduleParticipantsRemoval();
 
 app.post("/participants", (req, res) => {
-    const schema = Joi.object({
-        name: Joi.string().replace(/<|>/g, "").required().trim(),
-    });
-    const error = schema.validate(req.body).error;
-    if (error) {
+    if (checkNameError(req.body)) {
         res.sendStatus(400);
     } else {
         const cleanName = trimAndClean(req.body.name);
         if (participants.find((p) => p.name === cleanName)) {
             res.status(409).send("Esse nome já está sendo utilizado.");
         } else {
-            participants.push({
-                name: cleanName,
-                lastStatus: Date.now(),
-            });
-            messages.push({
-                from: cleanName,
-                to: "Todos",
-                text: "entra na sala...",
-                type: "status",
-                time: dayjs().format("HH:mm:ss"),
-            });
+            addParticipant(cleanName);
         }
         res.sendStatus(200);
         saveData();
@@ -51,31 +42,13 @@ app.get("/participants", (req, res) => {
 });
 
 app.post("/messages", (req, res) => {
-    const msgSchema = Joi.object({
-        to: Joi.string().replace(/<|>/g, "").required().trim(),
-        text: Joi.string().replace(/<|>/g, "").required().trim(),
-        type: Joi.string()
-            .pattern(new RegExp(/(^message$|^private_message$)/))
-            .required(),
-    });
-    const msgError = msgSchema.validate(req.body).error;
-    const headerSchema = Joi.object({
-        user: Joi.string().replace(/<|>/g, "").required().trim(),
-    }).unknown(true);
-    const headerError = headerSchema.validate(req.headers).error;
-    if (msgError || headerError) {
+    if (checkNewMsgError(req.body) || checkUserError(req.headers)) {
         res.sendStatus(400);
     } else {
         const cleanUser = trimAndClean(req.headers.user);
         const participant = participants.find((p) => p.name === cleanUser);
         if (participant) {
-            messages.push({
-                to: trimAndClean(req.body.to),
-                text: trimAndClean(req.body.text),
-                type: trimAndClean(req.body.type),
-                from: trimAndClean(req.headers.user),
-                time: dayjs().format("HH:mm:ss"),
-            });
+            addMessage(req);
             res.sendStatus(200);
             saveData();
         } else {
@@ -85,11 +58,7 @@ app.post("/messages", (req, res) => {
 });
 
 app.get("/messages", (req, res) => {
-    const schema = Joi.object({
-        user: Joi.string().replace(/<|>/g, "").required().trim(),
-    }).unknown(true);
-    const error = schema.validate(req.headers).error;
-    if (error) {
+    if (checkUserError(req.headers)) {
         res.sendStatus(400);
     } else {
         const cleanUser = trimAndClean(req.headers.user);
@@ -114,11 +83,7 @@ app.get("/messages", (req, res) => {
 });
 
 app.post("/status", (req, res) => {
-    const schema = Joi.object({
-        user: Joi.string().replace(/<|>/g, "").required().trim(),
-    }).unknown(true);
-    const error = schema.validate(req.headers).error;
-    if (error) {
+    if (checkUserError(req.headers)) {
         res.sendStatus(400);
     } else {
         const cleanUser = trimAndClean(req.headers.user);
@@ -137,22 +102,12 @@ app.listen(4000, () => {
     console.log("Running on port 4000");
 });
 
-function trimAndClean(string) {
-    return string.replace(/<|>/g, "").trim();
-}
-
-function scheduleParticipantsUpdate() {
+function scheduleParticipantsRemoval() {
     setInterval(() => {
         let saveTrigger = false;
         participants = participants.filter((p) => {
             if (Date.now() - p.lastStatus > 10000) {
-                messages.push({
-                    from: p.name,
-                    to: "Todos",
-                    text: "sai da sala...",
-                    type: "status",
-                    time: dayjs().format("HH:mm:ss"),
-                });
+                statusUserGotOut(p.name);
                 saveTrigger = true;
                 return false;
             } else {
@@ -170,4 +125,38 @@ function saveData() {
         "./data/chatData.json",
         JSON.stringify({ participants, messages })
     );
+}
+
+function addParticipant(username) {
+    participants.push({
+        name: username,
+        lastStatus: Date.now(),
+    });
+    messages.push({
+        from: username,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: dayjs().format("HH:mm:ss"),
+    });
+}
+
+function statusUserGotOut(username) {
+    messages.push({
+        from: username,
+        to: "Todos",
+        text: "sai da sala...",
+        type: "status",
+        time: dayjs().format("HH:mm:ss"),
+    });
+}
+
+function addMessage(req) {
+    messages.push({
+        to: trimAndClean(req.body.to),
+        text: trimAndClean(req.body.text),
+        type: trimAndClean(req.body.type),
+        from: trimAndClean(req.headers.user),
+        time: dayjs().format("HH:mm:ss"),
+    });
 }
