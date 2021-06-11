88617ef0e06fa08f5c082681edf1e4ea1ef0b762
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-06-10 23:19:07 -0300

    GET /messages with limit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const participants = [
     { name: "Vader", lastStatus: 12313123 },
-    { name: "Yoda", lastStatus: 12313123 },
+    { name: "Obi Wan", lastStatus: 12313123 },
 ];
 const messages = [
     {
@@ -34,9 +34,9 @@ const messages = [
         time: "20:06:14",
     },
     {
-        from: "Luke",
+        from: "Yoda",
         to: "Todos",
-        text: "Num pod c",
+        text: "chocado, eu estou",
         type: "message",
         time: "20:07:23",
     },
@@ -44,21 +44,23 @@ const messages = [
 
 app.post("/participants", (req, res) => {
     const schema = Joi.object({
-        name: Joi.string().required().trim(),
+        name: Joi.string()
+            .replace(/(<(.*?)>|>)/gi, "")
+            .required()
+            .trim(),
     });
-    const { error, validated } = schema.validate(req.body, {
+    const { error } = schema.validate(req.body, {
         abortEarly: false,
     });
     if (error) {
         const errorMsgs = error.details.map((e) => e.message);
         res.status(400).send(errorMsgs);
     } else {
-        console.log(validated);
-        if (participants.find((p) => p.name === req.body.name.trim())) {
+        if (participants.find((p) => p.name === trimAndClean(req.body.name))) {
             res.status(409).send("Esse nome já está sendo utilizado.");
         } else {
             participants.push({
-                name: req.body.name.trim(),
+                name: trimAndClean(req.body.name),
                 lastStatus: Date.now(),
             });
             res.send("OK");
@@ -70,13 +72,42 @@ app.get("/participants", (req, res) => {
     res.send(participants);
 });
 
-// app.post("/messages", (req, res) => {
+//app.post("/messages", (req, res) => {
 //
 // });
 
-// app.get("/messages", (req, res) => {
-//
-// });
+app.get("/messages", (req, res) => {
+    const schema = Joi.object({
+        user: Joi.string()
+            .replace(/(<(.*?)>|>)/gi, "")
+            .required()
+            .trim(),
+    }).unknown(true);
+    const { error } = schema.validate(req.headers, {
+        abortEarly: false,
+    });
+    if (error) {
+        const errorMsgs = error.details.map((e) => e.message);
+        res.status(400).send(errorMsgs);
+    } else {
+        const cleanUser = trimAndClean(req.headers.user);
+        const limit = req.query.limit && parseInt(req.query.limit);
+        if (participants.find((p) => p.name === cleanUser)) {
+            const filteredMessages = messages.filter(
+                (m) =>
+                    m.type === "message" ||
+                    m.to === cleanUser ||
+                    m.from === cleanUser
+            );
+            if (typeof limit === "number") {
+                filteredMessages.splice(0, filteredMessages.length - limit);
+            }
+            res.send(filteredMessages);
+        } else {
+            res.status(409).send("Usuário não logado.");
+        }
+    }
+});
 
 // app.post("/status", (req, res) => {
 //
@@ -85,3 +116,7 @@ app.get("/participants", (req, res) => {
 app.listen(4000, () => {
     console.log("Running on port 4000");
 });
+
+function trimAndClean(string) {
+    return string.replace(/(<(.*?)>|>)/gi, "").trim();
+}
